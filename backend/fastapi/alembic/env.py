9d04186c94758120df0c87f43358fa25b5718861
Tsushida-Ryuki <tsushida.3003@gmail.com>
2026-01-25 19:59:05 +0900

    ref: ruff check --fix

--- backend/fastapi/alembic/env.py
+++ backend/fastapi/alembic/env.py
@@ -1,10 +1,10 @@
 import os
 from logging.config import fileConfig
 
-from alembic import context
 from dotenv import load_dotenv
 from sqlalchemy import engine_from_config, pool
 
+from alembic import context
 from app.database import Base
 from app.models import Todo  # noqa: F401
 
